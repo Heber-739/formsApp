9697017a8a95ca46e5d4a233449d27f35f527293
Heber Duarte <heberduarteryr@gmail.com>
2023-07-11 11:01:53 -0300

    Basic structure html

---
+++ src/app/reactive/reactive.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ReactiveRoutingModule } from './reactive-routing.module';
+import { SwitchesComponent } from './pages/switches/switches.component';
+
+
+@NgModule({
+  declarations: [
+    SwitchesComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveRoutingModule
+  ]
+})
+export class ReactiveModule { }
